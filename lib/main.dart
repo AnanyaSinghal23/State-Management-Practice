404a5370f5a50f8f61eebd02bf6d9c6b6e767ac5
Ananya Singhal <singhalananya67@gmail.com>
2024-01-01 21:35:02 +0530

    date-time display

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main()
-{
-  runApp(const App(),);
+void main() {
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
 
 //package: hooks_riverpod
@@ -14,25 +17,43 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
+      // theme: ThemeData(
+      //   primaryColor: Colors.blue,
+      // ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
 }
 
+final currentDate = Provider(
+  (ref) => DateTime.now(),
+);
+
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(currentDate);
     return Scaffold(
-      appBar: AppBar(title: const Text('Home'),),
+      appBar: AppBar(
+        title: Center(child: const Text('Hooks Riverpod')),
+      ),
+      body: Center(
+          child: Text(
+        date.toString(),
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      )),
     );
   }
 }
+
+//**error** Bad state: provider to be available to ur app.. under runapp
 
 // class HomePage extends StatelessWidget {
 //   const HomePage({super.key});
